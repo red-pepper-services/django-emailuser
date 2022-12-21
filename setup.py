820244500c2fb827b0af37f9de18e032cb71fed0
Manuel <github@schiegg.at>
2022-12-21 14:48:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst', 'r') as readme:
 
 setup(
     name='django-cust-email-user',
-    version='0.1.0',
+    version='0.1.1',
     author='red-pepper-services',
     author_email='pypi@schiegg.at',
     url='https://github.com/red-pepper-services/django-emailuser',
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     classifiers = [
         'Development Status :: 3 - Alpha',
-        'Framework :: Wagtail',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
